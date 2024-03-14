2fc1e9549a0f1e0704f36a2a8f50dce326730097
testGajjar <163375920+testGajjar@users.noreply.github.com>
2024-03-14 10:11:40 +0530

    Add files via upload

---
+++ js/frontpage.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+	$('#content').on('click', '.find-post a', function(ev) {
+		ev.preventDefault();
+		
+		var url = $(this).data('url');
+		$('#content').load(url);
+	})
+});
